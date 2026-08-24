a8a6dc665f862d32bfad50c61c10ce58626eeaea
John Andrei L. Logmao <logmaojohnandrei9@gmail.com>
2026-08-24 22:33:56 +0800

    Project.jsx fix

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { projects } from "../data/projects";
+import { projects } from "../data/Projects";
 
 export default function Projects() {
   return (
